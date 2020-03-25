5da38fdbc8f55154f15c0ee4c7480b44090f3368
Ingo <ibogemann@whiteclarkegroup.com>
2020-03-25 15:08:22 +0000

    Initial commit 2

---
+++ public/js/emdr_controller.js
@@ -0,0 +1,73 @@
+const settings = {
+    bulbCount: 30,
+    background_color_on: "cyan",
+    background_color_off: "white",
+    height: "20px",
+    bulb_pause: 10,
+    edge_pause: 300,
+    mode:"blink`"
+}
+
+const state = {
+    curPos: 0,
+    direction: 1
+}
+
+async function next() {
+    setBulbState(state.curPos, false);
+    if (settings.mode === "blink"){
+        state.curPos=state.curPos===0?settings.bulbCount -1:0
+    }else{
+        state.curPos = state.curPos + state.direction;
+    }
+    setBulbState(state.curPos, true);
+    if (state.curPos == settings.bulbCount - 1 || state.curPos == 0) {
+        state.direction = -state.direction;
+        await sleep(settings.edge_pause);
+    }
+}
+
+
+function setBulbState(position, isOn) {
+    const lightbarNode = window.document.body.firstElementChild;
+    const bulbNode = lightbarNode.children.item(position);
+    bulbNode.style.backgroundColor = isOn ? settings.background_color_on : settings.background_color_off;
+}
+
+
+async function setUpLightbar() {
+    const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+    const count = 20;
+
+    const lightbarNode = window.document.body.firstElementChild;
+
+    lightbarNode.innerHTML = "";
+    for (let i = 0; i < settings.bulbCount; i++) {
+        lightbarNode.append(createLightNode());
+    }
+
+    while (true) {
+        await next();
+        await sleep(settings.bulb_pause);
+    }
+
+}
+
+function createLightNode() {
+    const lightNode = window.document.createElement("div");
+
+    lightNode.classList.add("light_bulb");
+    lightNode.style.height = settings.height;
+    lightNode.style.backgroundColor = settings.background_color_off;
+
+    return lightNode;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+window.onload = setUpLightbar
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'))
+
+
+app.listen(port, err => {
+    if (err) {
+        return console.error(err);
+    }
+    return console.log(`server is listening on ${port}`);
+});
