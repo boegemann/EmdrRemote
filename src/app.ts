ed84cccf1ae356d5d72eaefb90212d6e1a5a564c
Ingo <ibogemann@whiteclarkegroup.com>
2020-03-25 15:37:58 +0000

    Corrected to use heroku port if available

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,11 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
 
-app.listen(port, err => {
-    if (err) {
-        return console.error(err);
-    }
+app.listen(port, () => {
     return console.log(`server is listening on ${port}`);
 });
